3eb82b3cd8fbf9b11d6ab47f1cc13d9055592477
Shivanjali21 <shivanjaligupta10@gmail.com>
2024-06-08 21:06:48 +0530

    initial commit timer app.

---
+++ app/src/main/java/com/practice/meditationapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.practice.meditationapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.practice.meditationapp.ui.screens.TimerScreen
+import com.practice.meditationapp.ui.theme.MeditationAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationAppTheme {
+                //HomeScreen()
+                Surface(
+                    color = Color(0xFF101010),
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
+                        TimerScreen(
+                            totalTime = 100L * 1000L,
+                            handleColor = Color.Green,
+                            inActiveBarColor = Color.DarkGray,
+                            activeBarColor = Color(0xFF37B900),
+                            modifier = Modifier.size(200.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/practice/meditationapp/ui/data/BottomMenuData.kt
@@ -0,0 +1,8 @@
+package com.practice.meditationapp.ui.data
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuData (
+  val title: String,
+  @DrawableRes val iconId: Int,
+)
